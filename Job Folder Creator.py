b69dbdd34d0dee0f9f22bf3b9304d5205f06caa9
MikeN300 <miken300@gmail.com>
2022-08-10 16:29:09 -0400

    Debug and clean up code in folder creation

--- Job Folder Creator.py
+++ Job Folder Creator.py
@@ -5,24 +5,24 @@ while loopBack == True:
 	print('Which Server would you like to store this on? (1) Dunk (2) Funk')
 	#Defines variables for Funk or Dunk
 	whichServer = input()
-		if whichServer == 1:
-			path = '/Volumes/Dunk'
-			serverIP = 'afp://192.168.3.95'
-			shareName = 'OpenJobs'
-		elif whichServer == 2
-			path = '/Volumes/Funk'
-			serverIP = 'smb://192.168.3.96'
-			shareName = 'Projects'
-		else
-			print ('error: invalid selection please enter 1 for Dunk or 2 for Funk')
+	if whichServer == 1:
+		path = '/Volumes/Dunk'
+		serverIP = 'afp://192.168.3.95'
+		shareName = 'OpenJobs'
+	elif whichServer == 2:
+		path = '/Volumes/Funk'
+		serverIP = 'smb://192.168.3.96'
+		shareName = 'Projects'
+	else:
+		print ('error: invalid selection please enter 1 for Dunk or 2 for Funk')
 
 	#Checks server connection and attempts to connect if not available.
 	print('Checking if server is connected…')
 		
-	serverMounted = os.path.exists(path + "/" shareName)
-	if serverMounted = True:
+	serverMounted = os.path.exists(path + "/" + shareName)
+	if serverMounted == True:
 		break
-	elif serverMounted = False:
+	elif serverMounted == False:
 		try:
 			print('Mounting Server…')
 			#mount ServerIP
@@ -31,29 +31,30 @@ while loopBack == True:
 			print('Error mounting server ' + path + '. Please check your VPN connection and try again')
 			print('Would you like to (1) try again, or (2) exit?')
 			tryAgain = input()
-			if tryAgain == 1 :
+			if tryAgain == 1:
 				return #Will this return to elif statment?
 			elif tryAgain == 2:
 				exit
-			else
+			else:
 				print('Invalid Selection')
 	print('Server mounted!')
 
 	#Defines variables to create folders
-	print 'Enter Client Code (Enter it exactly as shown in the ' + shareName + ' folder):'
+	print('Enter Client Code (Enter it exactly as shown in the ' + shareName + ' folder):')
 	clientCode = input()
 	#Add code to check if Client Code folder exists on server, if not ask user if they want to os.mkdir)
-	print 'Enter Job Number:'
+	print('Enter Job Number:')
 	jobNumber = input()
-	print 'Enter Job Name:'
+	print('Enter Job Name:')
 	jobName = input()
 	print('Creating folders...')
 
 	#Function to creat folders
-	def makeFolders(folderName)
-		os.mkdir(path + '/' + clientCode + '/' + jobNumber + ' ' +  jobName + '/' jobNumber + '_' + folderName, mode = Oo777)
-
-	os.mkdir(path + '/' + clientCode + '/' + jobNumber +  jobName)
+	def makeFolders(folderName):
+		concatFoldersPath = path + '/' + clientCode + '/' + jobNumber + ' ' +  jobName + '/' + jobNumber + '_' + folderName
+		os.mkdir(concatFoldersPath, mode = Oo777)
+	concatJobPath = path + '/' + clientCode + '/' + jobNumber +  jobName
+	os.mkdir(concatJobPath)
 	makeFolders(PDF)
 	makeFolders(SRC)
 	makeFolders(Layout)
