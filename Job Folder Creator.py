ca6e6011e9f29a01dfa5fb49e6d0425bd66b3619
MikeN300 <miken300@gmail.com>
2022-08-10 15:53:21 -0400

    Correct Syntax and add While loop

--- Job Folder Creator.py
+++ Job Folder Creator.py
@@ -6,11 +6,11 @@ while loopBack == True:
 	#Defines variables for Funk or Dunk
 	whichServer=input()
 		if whichServer == 1:
-			path = /Volumes/Dunk
+			path = '/Volumes/Dunk'
 			serverIP = 'afp://192.168.3.95'
 			shareName = 'OpenJobs'
 		elif whichServer == 2
-			path = /Volumes/Funk
+			path = '/Volumes/Funk'
 			serverIP = 'smb://192.168.3.96'
 			shareName = 'Projects'
 		else
@@ -19,24 +19,25 @@ while loopBack == True:
 	#Checks server connection and attempts to connect if not available.
 	print('Checking if server is connected…')
 		
-		serverMounted = os.path.exists(path + "/" shareName)
-		if serverMounted = True:
-			break
-		elif serverMounted = False:
-			try:
-				print('Mounting Server…')
-				#mount ServerIP
-				serverMounted = True
-			except:
-				print('Error mounting server ' + path + '. Please check your VPN connection and try again')
-				print('Would you like to (1) try again, or (2) exit?')
-				tryAgain = input()
-				if tryAgain == 1 :
-					return #double return? Goto line 17?
-				elif tryAgain == 2:
-					exit
-				else
-					print('Invalid Selection')
+	serverMounted = os.path.exists(path + "/" shareName)
+	if serverMounted = True:
+		break
+	elif serverMounted = False:
+		try:
+			print('Mounting Server…')
+			#mount ServerIP
+			serverMounted = True
+		except:
+			print('Error mounting server ' + path + '. Please check your VPN connection and try again')
+			print('Would you like to (1) try again, or (2) exit?')
+			tryAgain = input()
+			if tryAgain == 1 :
+				return #double return? Goto line 17?
+			elif tryAgain == 2:
+				exit
+			else
+				print('Invalid Selection')
+	print('Server mounted!')
 
 	#Defines variables to create folders
 	print 'Enter Client Code (Enter it exactly as shown in the ' + shareName + ' folder):'
